2fd132565c7e98696a4c1b0eedd3837bfc2707e0
GwerhDev <gwerh.dev@gmail.com>
2024-06-07 13:10:39 -0400

    integrating redux

--- src/app/router/AppRouter.jsx
+++ src/app/router/AppRouter.jsx
@@ -2,12 +2,14 @@
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "../pages/LoginPage";
 import LandingPage from "../pages/LandingPage";
+import ContactPage from "../pages/ContactPage";
 
 export const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" Component={LandingPage} />
       <Route path="/login" Component={LoginPage} />
+      <Route path="/contact" Component={ContactPage} />
     </Routes>
   )
 }
