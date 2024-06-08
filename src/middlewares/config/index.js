b012cb5d2bb7e50e47167aa4e70ffadc7604c330
GwerhDev <gwerh.dev@gmail.com>
2024-06-07 20:12:34 -0400

    url api for production

--- src/middlewares/config/index.js
+++ src/middlewares/config/index.js
@@ -1,4 +1,4 @@
 import { environment } from "../../environment";
 import { DEVELOPMENT } from "../misc";
 
-export const URL_API = environment === DEVELOPMENT ? "http://localhost:8080" : "";
+export const URL_API = environment === DEVELOPMENT ? "http://localhost:8080" : "https://terralegal-api.vercel.app";
