68fb66f8745cdacf682a9136312487aa6445fa43
GwerhDev <gwerh.dev@gmail.com>
2024-06-13 14:00:37 -0400

    styling

--- src/app/components/Presentation/Presentation.jsx
+++ src/app/components/Presentation/Presentation.jsx
@@ -4,7 +4,7 @@ export const Presentation = () => {
   return (
     <div className={s.container}>
       <h2>Somos una empresa de Profesionales</h2>
-      <p>En TerraLegal nos especializamos en regularizaciones de terrenos en Peralillo y alrededores. Nuestro equipo está comprometido en brindar soluciones legales efectivas para sanear tu propiedad.</p>
+      <p>En TerraLegal nos especializamos en la regularización de terrenos en Peralillo y alrededores. Nuestro equipo está comprometido en brindar soluciones legales efectivas para sanear tu propiedad.</p>
     </div>
   )
 }
