6fc585998bd2f6a68772062230d8154a2a2603be
GwerhDev <gwerh.dev@gmail.com>
2024-06-14 16:45:34 -0400

    styling

--- src/app/pages/ContactPage.jsx
+++ src/app/pages/ContactPage.jsx
@@ -1,9 +1,11 @@
-import logo from '../../assets/images/jpg/logotype.jpg';
+import logo from '../../assets/images/jpg/logo.jpg';
 
 export default function ContactPage() {
   return (
     <div className="page-container contact-page">
-      <img src={logo} alt="" />
+      <img src={logo} alt="" width={"350px"} />
+      <h1 className="no-margin no-pading">TerraLegal</h1>
+      <p>Soluciones SpA</p>
     </div>
   )
 }
